3afe367e5464593a7ebde93763cf11ac7c46f1b9
Deepraj Baidya <deeprajbaidya@gmail.com>
2020-11-18 19:37:48 +0530

    Enabled User input for password Length

--- Password Manager/password.py
+++ Password Manager/password.py
@@ -6,9 +6,9 @@ upp="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 num="0123456789"
 sym="[]{}()!@#-$%^&*_,.+=""<>"
 
-main=low+upp+num+sym
+main=(low+upp+num+sym)
 
-length=16
+length=int(input("Enter the Length for your Password:--\n"))
 
 password="".join(random.sample(main,length))
 print(f"Your Password is:- {password}") 
